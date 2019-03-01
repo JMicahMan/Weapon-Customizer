4e66d75702d3415d4664baf141f567addb525900
JMicahMan <j.micah.manzanares@gmail.com>
2019-03-01 14:17:00 -0600

    eod

--- CustWeaps/Program.cs
+++ CustWeaps/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace CustWeaps
 {
@@ -12,9 +13,10 @@ namespace CustWeaps
 
         [STAThread]
         
+        
         static void Main()
         {
-          
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BaseWeapon());
